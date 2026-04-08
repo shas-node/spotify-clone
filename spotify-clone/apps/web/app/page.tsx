b7e1f3f87e24c7fe3e76a560613ffb550bdfce8d
shas-node <shas.sameer17@gmail.com>
2026-04-08 19:32:20 +0530

    added sound card

--- spotify-clone/apps/web/app/page.tsx
+++ spotify-clone/apps/web/app/page.tsx
@@ -1,10 +1,16 @@
 import Navbar from "@/components/Navbar"
 import Sidebar from "@/components/sidebar"
+import CardSongs from "@/components/songs"
 export default function Page() {
   return (
    <div className="min-h-screen">
     <Navbar/>
+    <main>
     <Sidebar/>
+    <div>
+      <CardSongs/>
+    </div>
+    </main>
 </div>
   )
 }
---
+++ spotify-clone/apps/web/components/songs.tsx
@@ -0,0 +1,67 @@
+import Image from "next/image"
+
+export default function CardSongs() {
+  return (
+    <div className="min-h-[90vh] bg-background my-15 p-4 lg:ml-80 mx-4 rounded-lg ">
+        <h2 className="text-white text-2xl mb-2 font-bold">New Songs</h2>
+        <div className="grid gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+        <div className="bg-background p-3 cursor-pointer hover:bg-hover rounded-md">
+        <Image src="/images/cover-2.jpeg" alt="cover-img" width={500} height={500} className="w-full h-50 rounded-md object-cover"></Image>
+        <p className="text-primary-text font-semibold">Rain on Marble Streets</p>
+         <p className="text-primary-text text-sm">The Lanterns</p>
+        </div>
+         <div className="bg-background p-3 cursor-pointer hover:bg-hover rounded-md">
+        <Image src="/images/cover-2.jpeg" alt="cover-img" width={500} height={500} className="w-full h-50 rounded-md object-cover"></Image>
+        <p className="text-primary-text font-semibold">Rain on Marble Streets</p>
+         <p className="text-primary-text text-sm">The Lanterns</p>
+        </div>
+         <div className="bg-background p-3 cursor-pointer hover:bg-hover rounded-md">
+        <Image src="/images/cover-2.jpeg" alt="cover-img" width={500} height={500} className="w-full h-50 rounded-md object-cover"></Image>
+        <p className="text-primary-text font-semibold">Rain on Marble Streets</p>
+         <p className="text-primary-text text-sm">The Lanterns</p>
+        </div>
+         <div className="bg-background p-3 cursor-pointer hover:bg-hover rounded-md">
+        <Image src="/images/cover-2.jpeg" alt="cover-img" width={500} height={500} className="w-full h-50 rounded-md object-cover"></Image>
+        <p className="text-primary-text font-semibold">Rain on Marble Streets</p>
+         <p className="text-primary-text text-sm">The Lanterns</p>
+        </div>
+         <div className="bg-background p-3 cursor-pointer hover:bg-hover rounded-md">
+        <Image src="/images/cover-2.jpeg" alt="cover-img" width={500} height={500} className="w-full h-50 rounded-md object-cover"></Image>
+        <p className="text-primary-text font-semibold">Rain on Marble Streets</p>
+         <p className="text-primary-text text-sm">The Lanterns</p>
+        </div>
+         <div className="bg-background p-3 cursor-pointer hover:bg-hover rounded-md">
+        <Image src="/images/cover-2.jpeg" alt="cover-img" width={500} height={500} className="w-full h-50 rounded-md object-cover"></Image>
+        <p className="text-primary-text font-semibold">Rain on Marble Streets</p>
+         <p className="text-primary-text text-sm">The Lanterns</p>
+        </div>
+         <div className="bg-background p-3 cursor-pointer hover:bg-hover rounded-md">
+        <Image src="/images/cover-2.jpeg" alt="cover-img" width={500} height={500} className="w-full h-50 rounded-md object-cover"></Image>
+        <p className="text-primary-text font-semibold">Rain on Marble Streets</p>
+         <p className="text-primary-text text-sm">The Lanterns</p>
+        </div>
+         <div className="bg-background p-3 cursor-pointer hover:bg-hover rounded-md">
+        <Image src="/images/cover-2.jpeg" alt="cover-img" width={500} height={500} className="w-full h-50 rounded-md object-cover"></Image>
+        <p className="text-primary-text font-semibold">Rain on Marble Streets</p>
+         <p className="text-primary-text text-sm">The Lanterns</p>
+        </div>
+         <div className="bg-background p-3 cursor-pointer hover:bg-hover rounded-md">
+        <Image src="/images/cover-2.jpeg" alt="cover-img" width={500} height={500} className="w-full h-50 rounded-md object-cover"></Image>
+        <p className="text-primary-text font-semibold">Rain on Marble Streets</p>
+         <p className="text-primary-text text-sm">The Lanterns</p>
+        </div>
+         <div className="bg-background p-3 cursor-pointer hover:bg-hover rounded-md">
+        <Image src="/images/cover-2.jpeg" alt="cover-img" width={500} height={500} className="w-full h-50 rounded-md object-cover"></Image>
+        <p className="text-primary-text font-semibold">Rain on Marble Streets</p>
+         <p className="text-primary-text text-sm">The Lanterns</p>
+        </div>
+         <div className="bg-background p-3 cursor-pointer hover:bg-hover rounded-md">
+        <Image src="/images/cover-2.jpeg" alt="cover-img" width={500} height={500} className="w-full h-50 rounded-md object-cover"></Image>
+        <p className="text-primary-text font-semibold">Rain on Marble Streets</p>
+         <p className="text-primary-text text-sm">The Lanterns</p>
+        </div>
+
+        </div>
+    </div>
+  )
+}
